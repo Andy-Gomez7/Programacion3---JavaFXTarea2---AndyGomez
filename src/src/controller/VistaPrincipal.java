c850eb61853110f80d24e9950a49b792de164b0c
Andy-Gomez7 <andyd.gomez496@gmail.com>
2026-07-12 19:16:24 -0400

    Actualizar ConsultaDestinos.fxml, actualizar ConsultaPaquetes.fxml, actualizar RegistroDestinos.fxml, actualizar RegistroPaquetes.fxml y actualizar VistaPrincipal.java

--- src/src/controller/VistaPrincipal.java
+++ src/src/controller/VistaPrincipal.java
@@ -1,5 +1,62 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+import utils.Navegacion;
+
 public class VistaPrincipal {
     
+    @FXML
+    private AnchorPane AnchorPane;
+
+    @FXML
+    private MenuItem ItemConsltDest;
+
+    @FXML
+    private MenuItem ItemSalir;
+
+    @FXML
+    private MenuBar MenuBar;
+
+    @FXML
+    private Menu SMenuConsultar;
+
+    @FXML
+    private Menu SMenuOtros;
+
+    @FXML
+    private Menu SMenuRegistro;
+
+    @FXML
+    private MenuItem itemConsltPaq;
+
+    @FXML
+    private MenuItem itemRegDest;
+
+    @FXML
+    private MenuItem itemRegPaq;
+
+    @FXML
+    public void AbrirRegistroPaquetes(){
+        Navegacion.abrirVentana("/view/RegistrodePaquetes.fxml", "Registro de paquetes");
+    }
+
+    @FXML
+    public void AbrirRegistroDestinos(){
+        Navegacion.abrirVentana("/view/RegistroDestinos.fxml", "Registro de destinos");
+    }
+
+    @FXML
+    public void AbrirConsultaPaquetes(){
+        Navegacion.abrirVentana("/view/ConsultaPaquetes.fxml", "Consulta de paquetes");
+    }
+
+    @FXML
+    public void AbrirConsultaDestinos(){
+        Navegacion.abrirVentana("/view/ConsultaDestinos.fxml", "Consulta de destinos");
+    }
 }
--- src/src/utils/ArchivoUtil.java
+++ src/src/utils/ArchivoUtil.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+
 import model.Destino;
 import model.Paquete;
 
@@ -17,7 +19,7 @@ public class ArchivoUtil {
         
     }
 
-    public void EscribirArch(Destino destino) {
+    public void EscribirDestino(Destino destino) {
         try{
             if(archivo.exists()){
                 FileWriter write = new FileWriter("Registro de destino.txt",true);
@@ -33,36 +35,71 @@ public class ArchivoUtil {
         }
     }
 
-    public void EscribirArch(Paquete paquete) {
+    public void EscribirPaquete(Paquete paquete) {
         try{
             if(archivo.exists()){
                 FileWriter write = new FileWriter("Registro de paquete.txt",true);
-                BufferedWriter bufWrite = new BufferedWriter(write);
 
-                bufWrite.write(paquete.GetCodigo()+";"+paquete.GetDestinatario()+";"+paquete.GetPeso()+";"+paquete.GetDestino());
+                write.write(paquete.GetCodigo()+";"+paquete.GetDestinatario()+";"+paquete.GetPeso()+";"+paquete.GetDestino());
 
-                bufWrite.close();
+                write.close();
             }         
         }catch(IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void LeerArch(){
+    public ArrayList<Destino> LeerDestino(){
            try{
+            ArrayList<Destino> destinos = new ArrayList<>();
             FileReader read = new FileReader("Leer Registro.txt");
             BufferedReader bufread = new BufferedReader(read);
 
             String linea;
 
             while((linea =  bufread.readLine()) != null){
-                System.out.println(linea);
-                System.out.println();
+                Destino Destino = new Destino(linea);
+                destinos.add(Destino);
             }
+
+            read.close();
             bufread.close();
+
+            return destinos;
+            
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public ArrayList<Paquete> LeerPaquete(){
+           try{
+            FileReader read = new FileReader("Leer Registro.txt");
+            BufferedReader bufread = new BufferedReader(read);
+            ArrayList<Paquete> paquetes = new ArrayList<>();
+            String[] aux;
+
+            String linea;
+
+            while((linea =  bufread.readLine()) != null){
+                aux = linea.split(";");
+                if(aux.length == 4){
+                    Paquete paquete = new Paquete(aux[0], aux[1], Double.parseDouble(aux[2]), aux[3]);
+                    paquetes.add(paquete);
+                }
+            }
+
+            read.close();
+            bufread.close();
+            
+            return paquetes;
 
         }catch(Exception e){
             e.printStackTrace();
         }
+        
+        return null;
     }
 }

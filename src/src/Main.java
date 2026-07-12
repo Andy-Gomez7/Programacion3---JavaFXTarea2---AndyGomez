78ab8fbb47fc24a11ca076e9dd76fc2878c761ac
Andy-Gomez7 <andyd.gomez496@gmail.com>
2026-07-12 03:57:56 -0400

    Agregar ConsultaDestinos.fxml, agregar ConsultaPaquetes.fxml, agregar RegistroDestinos.fxml y agregar RegistroPaquetes.fxml

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,5 +1,6 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -8,7 +9,13 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage arg0) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/registro.fxml"));
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/VistaPrincipal.fxml"));
+
+        Scene scene = new Scene(loader.load());
+
+        primaryStage.setTitle("Sistema de paquetes y destinos");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
